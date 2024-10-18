2d2f7508ed8d0abefac82f5e95bb5951cf67989f
7vTr0x7 <7vTrox7@gmail.com>
2024-10-18 10:31:51 +0530

    server

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import { initializeDatabase } from "./db/db.connection.js";
+
+const app = express();
+
+app.use(express.json());
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+initializeDatabase();
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server Running On PORT: ${PORT}`);
+});
