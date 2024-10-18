6696e2767a0ef2399d0cefd854da1f37fb06de13
7vTr0x7 <7vTrox7@gmail.com>
2024-10-18 12:30:57 +0530

    PostCard

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -4,13 +4,35 @@ import { FaRegHeart } from "react-icons/fa";
 import { FaRegComment } from "react-icons/fa";
 import { RiShareForwardLine } from "react-icons/ri";
 import { FaRegBookmark } from "react-icons/fa6";
+import { RxCross2 } from "react-icons/rx";
+import { Toaster, toast } from "react-hot-toast";
 
 const PostCard = ({ post }) => {
   const [isFullText, setIsFullText] = useState(false);
+  const [isOptionOpen, setIsOptionOpen] = useState(false);
+
+  const deleteHandler = async ({ postId }) => {
+    try {
+      const res = await fetch(`http://localhost:4000/api/post/${postId}`, {
+        method: "DELETE",
+      });
+
+      if (!res.ok) {
+        console.log("Failed to get posts");
+      }
+
+      const data = await res.json();
+      if (data) {
+        toast.success("Post Deleted");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <div className="bg-light rounded-3 shadow-lg p-4">
-      <div className="d-flex justify-content-between align-items-center">
+    <div className="bg-light  rounded-3 shadow-lg p-4">
+      <div className="relative d-flex justify-content-between align-items-center">
         <div className="d-flex  align-items-center">
           <img
             alt={post.user.name}
@@ -20,8 +42,29 @@ const PostCard = ({ post }) => {
           />
           <p className="m-0 px-3 fw-semibold">{post.user.name}</p>
         </div>
-        <BsThreeDotsVertical />
+        {isOptionOpen && (
+          <div
+            className="absolute px-3 py-2 w-25 shadow-lg rounded-3 fw-semibold"
+            style={{ right: "0px", top: "20px", cursor: "pointer" }}>
+            <p className="m-0">Edit</p>
+            <p className="m-0" onClick={() => deleteHandler(post._id)}>
+              Delete
+            </p>
+          </div>
+        )}
+        <div onClick={() => setIsOptionOpen((prev) => !prev)}>
+          {isOptionOpen ? (
+            <p className="m-0" style={{ height: "30px", cursor: "pointer" }}>
+              <RxCross2 />
+            </p>
+          ) : (
+            <p className="m-0" style={{ height: "30px", cursor: "pointer" }}>
+              <BsThreeDotsVertical />
+            </p>
+          )}
+        </div>
       </div>
+
       <div>
         <p className="my-2 fw-semibold">{post.title}</p>
         <p className="my-2 ">
@@ -54,6 +97,7 @@ const PostCard = ({ post }) => {
         </div>
         <FaRegBookmark />
       </div>
+      <Toaster />
     </div>
   );
 };
