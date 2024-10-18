6a93b8f96ef101d5ff6ef98be224761ab1ce6607
7vTr0x7 <7vTrox7@gmail.com>
2024-10-18 11:22:16 +0530

    Posts

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
   return <div className="container my-5 d-flex justify-content-center"></div>;
 };
 
