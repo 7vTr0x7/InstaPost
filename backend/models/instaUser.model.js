9f2d874cb79cda9d9ae6c263dcae2c445fa2a135
7vTr0x7 <7vTrox7@gmail.com>
2024-10-18 10:27:29 +0530

    instaUser

---
+++ backend/models/instaUser.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const InstaUserSchema = new mongoose.Schema({
+  
+    name: {
+        type:String,
+        required:true
+    },
+    imageURL: {
+        type:String,
+        required:true
+    }
+
+}, {timestamps:true});
+
+const InstaUser = mongoose.model('InstaUser', InstaUserSchema);
+module.exports = InstaUser;
