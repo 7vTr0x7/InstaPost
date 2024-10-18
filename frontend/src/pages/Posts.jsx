ca0f0b657704fc66120f0e31cd66b95c43c791f3
7vTr0x7 <7vTrox7@gmail.com>
2024-10-18 11:30:51 +0530

    Posts

--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,7 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
+
+  const user = useSelector((state) => state.user.user);
+
+  const fetchPosts = async () => {
+    try {
+        const res = await fetch(`http://localhost:4000/api/posts`,{
+            method:
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
 
   return <div className="container my-5 d-flex justify-content-center"></div>;
 };
