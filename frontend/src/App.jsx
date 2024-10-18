f8d92ec5b76d3cfa5ef44c9066df570dc17ef52e
7vTr0x7 <7vTrox7@gmail.com>
2024-10-18 11:15:25 +0530

    routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Posts from "./pages/Posts";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Posts />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
